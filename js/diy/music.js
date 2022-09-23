b8e0540b3209d7e04a815d4bfa6f352f9a21c2fd
agilebeanisartist@gmail.com <agilebeanisartist@gmail.com>
2022-09-24 05:24:39 +0800

    Site updated: 2022-09-24 05:24:38

--- js/diy/music.js
+++ js/diy/music.js
@@ -3,8 +3,7 @@ const ap = new APlayer({
     fixed: false,
     autoplay: true, //自动播放
     listFolded: false, //列表默认折叠
-    lrcType: 3, //歌词传递方式
-    mutex: true,
+    mutex: false,
     audio: [
      {
         name: "Mine Mine",
@@ -15,8 +14,8 @@ const ap = new APlayer({
     {
         name: "家后",
         artist: '江蕙',
-        url: 'https://m704.music.126.net/20220924051333/4874589dd3f17e4f789c88419cb28030/jdyyaac/005c/545d/045f/76aa00530fc7752e9b6b35b325445622.m4a?authSecret=000001836c1bed2e0c0b0aaba0a20bcf',
-        cover: 'https://p1.music.126.net/nLnXSnnvHt8ovLxbfX5kXA==/89060441862577.jpg?param=130y130',
+        url: 'https://dl.stream.qqmusic.qq.com/C400002J5tpL1DPD7C.m4a?guid=962252980&vkey=644F520711624E763AC67C44B4142992B84682127BBB475225E03425FCE3B0C9B3CA431A66582AB26C6BD62DBCE8D6F2FA55098AA15E1074&uin=1152921505000885401&fromtag=120032',
+        cover: 'https://y.qq.com/music/photo_new/T002R300x300M000000Te3Hp0toVGn_2.jpg?max_age=2592000',
         
     },
     {
@@ -25,6 +24,15 @@ const ap = new APlayer({
         url: 'http://www.ytmp3.cn/down/75603.mp3',
         cover: 'https://y.qq.com/music/photo_new/T002R300x300M000000P4Dod0GzRlM.jpg?max_age=2592000',
     },
+    {
+        name: "我很忙",
+        artist: 'A-Lin',
+        url: 'https://dl.stream.qqmusic.qq.com/C4000003NJrj1mcTKN.m4a?guid=4519797206&vkey=2F248DD2157921017BAF40CB9E39461983CD13D1E5824A2DAA5352C4A8A5E1DAFA9A506DEBF47CD5C2355A744F411D090351E470623AE6EF&uin=1152921505000885401&fromtag=120032',
+        cover: 'https://y.qq.com/music/photo_new/T002R300x300M000004Byln34HMGEB.jpg?max_age=2592000',
+            },
     ]
 });
+
+
+
 
